f304ce47116b308af7ad47c43d5f7572970bc7ab
konstantin bondarenko <zois1990@gmail.com>
2022-08-12 16:10:45 +0200

    edit all task with methods

--- Program.cs
+++ Program.cs
@@ -2,46 +2,53 @@
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 /*
-Console.Write("Input 3-digit number: ");
-int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-int result;
-if (number > 99 && number < 1000)
+void showSecondDigit(int number)
 {
-    result = number / 10 % 10;
-    Console.WriteLine(result);
+    int result;
+    if (number > 99 && number < 1000)
+    {
+        result = number / 10 % 10;
+        Console.WriteLine(result);
+    }
+    else
+        Console.WriteLine("Not 3-digit number");
 }
-else
-    Console.WriteLine("Not 3-digit number");
+
+Console.Write("Input 3-digit number: ");
+showSecondDigit(Math.Abs(Convert.ToInt32(Console.ReadLine())));
 */
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 /*
-Console.Write("Input number: ");
-long number = Math.Abs(Convert.ToInt64(Console.ReadLine()));
-byte thirdDigit;
-if (number < 100)
-    Console.WriteLine("No 3rd digit");
-else
+void showThirdDigit (int number)
 {
-    for (; number > 999; number = number / 10);
-    thirdDigit = Convert.ToByte(number % 10);
-    Console.WriteLine(thirdDigit);
+    byte thirdDigit;
+    if (number < 100)
+        Console.WriteLine("No 3rd digit");
+    else
+    {
+        for (; number > 999; number = number / 10);
+        thirdDigit = Convert.ToByte(number % 10);
+        Console.WriteLine(thirdDigit);
+    }
 }
+Console.Write("Input number: ");
+showThirdDigit(Math.Abs(Convert.ToInt32(Console.ReadLine())));
 */
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 /*
+bool weekend (int number)
+{
+    return number == 6 || number == 7;
+}
+
 Console.Write("Input day of the week: ");
-byte number = Convert.ToByte(Console.ReadLine());
-if (number < 1 || number > 7)
-    Console.WriteLine("It's not correct day");
-else if (number > 5)
-    Console.WriteLine("It's Weekend!");
-else
-    Console.WriteLine("Go to work");
+Console.WriteLine(weekend(Convert.ToInt32(Console.ReadLine())));
 */
+
 
 
 
